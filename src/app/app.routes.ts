b29e58c919e6bcbd6ca4cf9fc7cbac068b824d57
Elena <elena@192.168.1.8>
2025-12-24 17:21:23 +0100

    feat: add user profile page UI and icons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { LoginComponent } from './core/auth/pages/login/login.component';
 import { RegisterComponent } from './core/auth/pages/register/register.component';
+import { UserProfileComponent } from './features/user-profile/user-profile.component';
+import { DriverProfileComponent } from './features/driver-profile/driver-profile.component';
+import { AdminProfileComponent } from './features/admin-profile/admin-profile.component';
 
 export const routes: Routes = [
   {
@@ -15,5 +18,17 @@ export const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent,
+  },
+  {
+    path: 'profile',
+    component: UserProfileComponent
+  },
+  {
+    path: 'driver-profile',
+    component: DriverProfileComponent
+  },
+  {
+    path: 'admin-profile',
+    component: AdminProfileComponent
   },
 ];
