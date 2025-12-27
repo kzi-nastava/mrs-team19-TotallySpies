515ce872cb1be368f0b02925ada6fe14da3a3f6e
Andjela Broceta <andjela.broceta.bn@gmail.com>
2025-12-27 15:44:26 +0100

    feature:implement registration on mobile client side

--- mobile/app/src/main/java/com/ftn/mobile/presentation/view/Register.java
+++ mobile/app/src/main/java/com/ftn/mobile/presentation/view/Register.java
@@ -1,4 +1,4 @@
-package com.ftn.mobile;
+package com.ftn.mobile.presentation.view;
 
 import android.os.Bundle;
 
@@ -8,13 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Login extends AppCompatActivity {
+import com.ftn.mobile.R;
+
+public class Register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
