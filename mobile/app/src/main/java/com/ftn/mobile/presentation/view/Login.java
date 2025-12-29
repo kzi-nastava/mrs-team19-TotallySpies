1cc1484b74923a5b29905f5fb6a4938945f56155
Sara Cvjetkovic <saracvjetkovic2005@gmail.com>
2025-12-29 11:30:07 +0100

    feature: added navbar for mobile app

--- mobile/app/src/main/java/com/ftn/mobile/presentation/view/Login.java
+++ mobile/app/src/main/java/com/ftn/mobile/presentation/view/Login.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 
 import com.ftn.mobile.R;
@@ -15,6 +18,10 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login_scroll_view), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
