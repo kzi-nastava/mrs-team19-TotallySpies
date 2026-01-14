e54b724825451431a82668f0eebc95fb269678da
Sara Cvjetkovic <saracvjetkovic2005@gmail.com>
2026-01-14 14:09:06 +0100

    feature: added map on ride tracking page for user

--- frontend/src/app/features/ride-tracker-user/ride-tracker-user.component.ts
+++ frontend/src/app/features/ride-tracker-user/ride-tracker-user.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DriverInfoComponent } from '../../shared/components/driver-info/driver-info.component';
 import { TimerCardComponent } from '../../shared/components/timer-card/timer-card.component';
+import { MapComponent } from '../../shared/components/map/map.component';
 
 @Component({
   selector: 'app-ride-tracker-user',
-  imports: [DriverInfoComponent, TimerCardComponent],
+  imports: [DriverInfoComponent, TimerCardComponent, MapComponent],
   templateUrl: './ride-tracker-user.component.html',
   styleUrl: './ride-tracker-user.component.css',
 })
@@ -18,5 +19,7 @@ export class RideTrackerUserComponent {
 
   minutesRemaining: number = 7;
   progress: number = 70;
+  pickupAddress: string = 'Micurinova 40, Novi Sad';
+  destinationAddress: string = 'Strazilovska 7, Novi Sad';
 
 }
