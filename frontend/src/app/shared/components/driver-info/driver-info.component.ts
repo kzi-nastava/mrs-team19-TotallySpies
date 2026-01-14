15b150afe6a7b11ab756c773d9ea997243973de4
Sara Cvjetkovic <saracvjetkovic2005@gmail.com>
2026-01-14 21:34:51 +0100

    feature: add vehicle and driver rating

--- frontend/src/app/shared/components/driver-info/driver-info.component.ts
+++ frontend/src/app/shared/components/driver-info/driver-info.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { ReportDialogComponent } from '../../report-dialog/report-dialog.component';
+import { ReviewDialogComponent } from '../../review-dialog/review-dialog.component';
 
 @Component({
   selector: 'app-driver-info',
@@ -15,6 +16,7 @@ export class DriverInfoComponent {
   @Input() carModel: string = 'Subaru XV';
   @Input() rating: number = 5;
   @Input() profileImageUrl: string = '/images/driver_placeholder.png';
+  @Input() isFinished: boolean = false;
   constructor(private dialog: MatDialog) {}
 
   openReport() {
@@ -23,5 +25,9 @@ export class DriverInfoComponent {
       panelClass: 'custom-dialog-container',
       backdropClass: 'blur-backdrop'
     });
+  }
+
+  openReview(type: 'driver' | 'vehicle'): void {
+    this.dialog.open(ReviewDialogComponent, { width: '25rem', data: { type }, panelClass: 'custom-dialog-container', backdropClass: 'blur-backdrop' });
   }
 }
