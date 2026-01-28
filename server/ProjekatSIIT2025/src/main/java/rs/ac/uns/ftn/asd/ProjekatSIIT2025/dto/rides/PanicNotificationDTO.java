05fedfbba74af4c880bfc9ab69c65fd38f9dff69
Andjela Broceta <andjela.broceta.bn@gmail.com>
2026-01-28 13:40:17 +0100

    feature: implement front back connection for panic button

--- server/ProjekatSIIT2025/src/main/java/rs/ac/uns/ftn/asd/ProjekatSIIT2025/dto/rides/PanicNotificationDTO.java
+++ server/ProjekatSIIT2025/src/main/java/rs/ac/uns/ftn/asd/ProjekatSIIT2025/dto/rides/PanicNotificationDTO.java
@@ -3,24 +3,14 @@ package rs.ac.uns.ftn.asd.ProjekatSIIT2025.dto.rides;
 import java.time.LocalDateTime;
 
 public class PanicNotificationDTO {
-    private Long userId;
     private Long rideId;
     LocalDateTime time;
     String reason;
 
-    public PanicNotificationDTO(Long userId, Long rideId, LocalDateTime time, String reason) {
-        this.userId = userId;
+    public PanicNotificationDTO( Long rideId, LocalDateTime time, String reason) {
         this.rideId = rideId;
         this.time = time;
         this.reason = reason;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
     }
 
     public Long getRideId() {
